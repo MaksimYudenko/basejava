57cfe99787ad445b6743d427c9bdcd6fab047212
Maksim Yudenko <yudenkomaksim@gmail.com>
2017-11-10 23:49:00 +0300

    Lesson 08. Display file and dir names by recursion.

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,37 +1,19 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
-/**
- * gkislin
- * 21.07.2016
- */
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
-
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-
-        File dir = new File("./src/ru/javawebinar/basejava");
-        System.out.println(dir.isDirectory());
-        String[] list = dir.list();
-        if (list != null) {
-            for (String name : list) {
-                System.out.println(name);
-            }
-        }
-
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        getFiles(new File("."));
     }
+
+    private static void getFiles(File file) {
+        if (file.isDirectory()) {
+            try {System.out.println(file.getCanonicalPath());}
+            catch (IOException e) {e.printStackTrace();}
+            for (File dir : file.listFiles()) getFiles(dir);
+        } else {System.out.println("\t" + file.getName());}
+    }
+
 }
